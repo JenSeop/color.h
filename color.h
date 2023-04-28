a084f92442e6081599ca1a62179a9cb548c9b0ff
jungskim <dostwoman@gmail.com>
2023-04-28 12:36:47 +0900

    [UPLOAD][230428] color.h

---
+++ color.h
@@ -0,0 +1,16 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+#define BLACK "\033[0;30m" // COLOR : BLACK
+#define RED "\033[0;31m" // COLOR : RED
+#define GREEN "\033[0;32m" // COLOR : GREEN
+#define YELLOW "\033[0;33m" // COLOR :  YELLOW
+#define BLUE "\033[0;34m" // COLOR : BLUE
+#define PURPLE "\033[0;35m" // COLOR : PURPLE
+#define SKY "\033[0;36m" // COLOR : CYAN | SKY BLUE
+#define WHITE "\033[0;37m" // COLOR : WHITE
+#define BASIC "\033[0;37m" // COLOR : BASIC
+#define ERR "\033[0;31m" // COLOR : ERR
+#define COL "\033[0;32m" // COLOR : COL
+
+#endif
